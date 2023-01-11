82edaeb60cf2f1e44d8040991d3e0b6901d6e399
mahmoud-ahmed-elqenawey <moodiiy.98@gmail.com>
2023-01-11 17:05:51 +0200

    initial framer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion, useScroll, useSpring } from "framer-motion";
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001,
+    });
+
+    return (
+        <div className="App">
+            <motion.div className="progress-bar" style={{ scaleX }} />
+        </div>
+    );
 }
 
 export default App;
